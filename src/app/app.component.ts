1fa01ffb4e446afcb44a8d54bb3ce27962e2de1a
DimitriTsikaridze <dimitri.tsikaridze.1@btu.edu.ge>
2022-11-19 19:06:59 -0800

    add assets and default styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from "@angular/core"
+import { HttpClient } from "@angular/common/http"
+import { ChangeDetectionStrategy, Component } from "@angular/core"
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+}
